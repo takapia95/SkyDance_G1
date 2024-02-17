af5a64ca5c99ba0a87dbe7a070099972642ae0cb
Ai Tran <trankhaphia5@gmail.com>
2024-02-17 07:03:20 +0000

    added features into Photo

--- sky-dance/src/components/NavBar.js
+++ sky-dance/src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-function Home() {
+function NavBar() {
  return (
     <div>
 
-        <Link to="/photos">Look up in the sky, it's a bird, it's a plane</Link>
+        <Link to="/">Take me home</Link>
     </div>
  );
 
 }
-export default Home;
+export default NavBar;
--- sky-dance/src/components/Photos.js
+++ sky-dance/src/components/Photos.js
@@ -1,26 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from "./NavBar"
+const apiKey = process.env.REACT_APP_NASA_KEY;
 
 export default function Photos() {
     const [photoData, setPhotoData] = useState(null);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         async function fetchPhoto() {
-            const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=H5g86kW7KlmRQnshr5zGkqUYi2OorIZmi4KL2s5d');
-            const data = await res.json();
-            setPhotoData(data);
-            console.log(data);
+            try {
+                const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=H5g86kW7KlmRQnshr5zGkqUYi2OorIZmi4KL2s5d`);
+                if (!res.ok) {
+                    throw new Error('Failed to fetch data from the NASA API');
+                }
+                const data = await res.json();
+                setPhotoData(data);
+                console.log(data);
+            } catch (error) {
+                setError(error.message);
+            }
         }
 
-        fetchPhoto(); // Call the fetchPhoto function
+        fetchPhoto();
     }, []);
 
+    if (error) return <p>Error: {error}</p>;
     if (!photoData) return <p>Loading...</p>;
+    
     return (
+        <>
+        <NavBar />
         <div>
-            <img
-                src={photoData.hdurl} 
-                alt={photoData.title}
-            />
+            {photoData.media_type === "image" ? (
+                <img
+                    src={photoData.hdurl} 
+                    alt={photoData.title}
+                />
+            ) : ( 
+                <iframe
+                    title="space-video"
+                    src={photoData.hdurl}
+                    frameBorder="0"
+                    gesture="media"
+                    allow="encrypted-media"
+                    allowFullScreen
+                    className="photo"
+                />
+            )}
+
+            <h1>{photoData.title}</h1>
+            <h5>{photoData.date}</h5>
+            <p>{photoData.explanation}</p>
         </div>
+        </>
     );
 }
